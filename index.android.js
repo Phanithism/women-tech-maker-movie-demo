a54fe281efa38eaad976620f495cbb98f62d2dc4
Panh Phanith <panhphanith.kh@gmail.com>
2017-04-29 14:16:55 +0700

    finish movie list

--- index.android.js
+++ index.android.js
@@ -9,45 +9,39 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  ListView
 } from 'react-native';
 
+import axios from 'axios';
+
 export default class movieDemo extends Component {
+
+  constructor() {
+    super();
+    this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: this.ds.cloneWithRows([]),
+    };
+  }
+
+  componentWillMount () {
+    axios.get('https://yts.ag/api/v2/list_movies.json').then(response => {
+      console.log(response.data.data.movies);
+      this.setState({dataSource: this.ds.cloneWithRows(response.data.data.movies)})
+    })
+
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <ListView
+        enableEmptySections={false}
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => <Text>Title: {rowData.title} - Rating: {rowData.rating}</Text>}
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('movieDemo', () => movieDemo);
